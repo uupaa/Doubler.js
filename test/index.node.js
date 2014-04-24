0046896f42b7112c8dff06582fe9a0cb7859e31c
uupaa <uupaa.js@gmail.com>
2014-04-25 03:34:35 +0900

    update setting

--- test/index.node.js
+++ test/index.node.js
@@ -1,10 +1,10 @@
-require("uupaa.valid.js");
-require("uupaa.task.js");
-require("uupaa.test.js");
-require("uupaa.wordarray.js");
-require("uupaa.bytearray.js");
-require("uupaa.base64.js");
+require("../node_modules/uupaa.nodemodule.js/lib/NodeModule.js");
+require("../node_modules/uupaa.valid.js/lib/Valid.js");
+require("../node_modules/uupaa.task.js/lib/Task.js");
+require("../node_modules/uupaa.test.js/lib/Test.js");
+require("../node_modules/uupaa.wordarray.js/lib/WordArray.js");
+require("../node_modules/uupaa.base64.js/node_modules/uupaa.bytearray.js/lib/ByteArray.js");
+require("../node_modules/uupaa.base64.js/lib/Base64.js");
 require("../lib/Doubler.js");
 require("../release/Doubler.min.js");
 require("./test.js");
-
