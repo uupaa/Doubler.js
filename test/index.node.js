d5464c3a632430596de95c738d90363f5850722d
uupaa <uupaa.js@gmail.com>
2014-04-28 12:14:51 +0900

    follow NodeModule.js

--- test/index.node.js
+++ test/index.node.js
@@ -1,5 +1,5 @@
-require("../node_modules/uupaa.valid.js/lib/Valid.js");
 require("../node_modules/uupaa.nodemodule.js/lib/NodeModule.js");
+require("../node_modules/uupaa.valid.js/lib/Valid.js");
 require("../node_modules/uupaa.task.js/lib/Task.js");
 require("../node_modules/uupaa.test.js/lib/Test.js");
 require("../node_modules/uupaa.wordarray.js/lib/WordArray.js");
